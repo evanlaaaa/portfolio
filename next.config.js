42d21a425e6b5d2d46e58be8e118f5bd6be908b2
evanlaaaa <xiaoxinad@hotmail.com>
2022-04-19 17:19:28 +0800

    Add basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
