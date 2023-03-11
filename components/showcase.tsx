311b01bafda6b7b478fedaadb0038d075e9f9a43
evanlaaaa <xiaoxinad@hotmail.com>
2023-03-11 11:55:30 +0800

    Fix typo

--- components/showcase.tsx
+++ components/showcase.tsx
@@ -46,7 +46,7 @@ export const Showcase = () => {
       projectName: "Reverse Engineer MT-Framework *.ipr file",
       projectDescription: "Yet another small project to burn times. Developed a Python addon for Blender to import 3D models from binary *.ipr files. Used 010 Editor to analyze the binary file structure and extract relevant data. Created custom import functions in Python that converted the extracted data into a format compatible with Blender's API",
       imageUrl: '/ipr.png',
-      technologies: ["Python", "010 template", "Blender"],
+      technologies: ["Python", "010 Editor", "Blender"],
       gallery: ["/ipr/1.png", "/ipr/2.png"],
       external: [
         new External({
